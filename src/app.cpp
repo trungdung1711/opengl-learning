e1126037773bcd9f331b5f234f5273a6199bf424
trungdung1711 <trungdunglebui17112004@gmail.com>
2024-07-27 00:50:37 +0700

    dung-making the ./src/app.cpp cleaner

--- src/app.cpp
+++ src/app.cpp
@@ -48,31 +48,6 @@ int main(void)
     std::cout <<"Number of texture uints: " << n << '\n';
 
     stbi_set_flip_vertically_on_load(true); 
-    
-    
-    float vertices_reg[]
-    {
-         0.0f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
-        -0.5f,  0.0f, 0.0f, 0.0f, 1.0f, 0.0f,
-         0.0f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
-         0.5f,  0.0f, 0.0f, 1.0f, 0.0f, 1.0f
-    };
-
-
-    GLuint indices[]
-    {
-        0,1,3,
-        1,2,3
-    };
-
-
-    float vertices_ex[]
-    {
-        0.0f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
-       -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f,
-        0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,
-    };
-
 
     float text_vertices[]
     {
@@ -94,112 +69,7 @@ int main(void)
     /**
      * Use of the new version of shader object
      */
-    util::Shader s1{"D:/trungdung/C++/OpenGL/src/glsl/vertex_shader.glsl","D:/trungdung/C++/OpenGL/src/glsl/fragment_shader.glsl"};
-    util::Shader s2{"D:/trungdung/C++/OpenGL/src/glsl/vertex_shader_ex.glsl","D:/trungdung/C++/OpenGL/src/glsl/fragment_shader_ex.glsl"};
     util::Shader s3{"../src/glsl/text_vertex_shader.glsl","../src/glsl/text_fragment_shader.glsl"};
-    /**
-     * Tell openGL how to interpret the vertex data using attribute pointer
-     * Enable the vertex attribute to get the arguments
-     * Ready for the graphics pipeline
-     */
-
-
-    /**
-     * Use VAO to remember all the settings of VBO
-     * Generate a VAO
-     * Bind the VAO to the openGL context
-     * Also give openGL, VAO how to interpret the vertex data
-     * Every configuring of the VBO -> stored inside the VAO for later use
-     */
-    // GLuint VAO;
-    // glGenVertexArrays(1,&VAO);
-    // glBindVertexArray(VAO);
-    // /**
-    //  * Get the ID for the VBO
-    //  * Return the ID for this buffer
-    //  * Bind this buffer with the GL_ARRAY_BUFFER
-    //  * Transmit the data to the GPU's memory
-    //  */
-    // unsigned int VBO;
-    // glGenBuffers(1,&VBO);
-    // glBindBuffer(GL_ARRAY_BUFFER,VBO);
-    // glBufferData(GL_ARRAY_BUFFER, sizeof(vertices),vertices,GL_STATIC_DRAW);
-    // glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,sizeof(float)*3,(void*)0);
-    // glEnableVertexAttribArray(0);
-
-    // glBindVertexArray(0);
-
-    // GLuint vao[2];
-    // glGenVertexArrays(2,vao);
-
-    // GLuint vbo[2];
-    // glGenBuffers(2,vbo);
-
-    // glBindVertexArray(vao[0]);
-
-    // glBindBuffer(GL_ARRAY_BUFFER,vbo[0]);
-    // glBufferData(GL_ARRAY_BUFFER,sizeof(vertices_A),vertices_A,GL_STATIC_DRAW);
-    // glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,sizeof(float)*3,(void*)0);
-    // glEnableVertexAttribArray(0);
-
-    // glBindVertexArray(0);
-
-    // glBindVertexArray(vao[1]);
-
-    // glBindBuffer(GL_ARRAY_BUFFER,vbo[1]);
-    // glBufferData(GL_ARRAY_BUFFER,sizeof(vertices_B),vertices_B,GL_STATIC_DRAW);
-    // glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,sizeof(float)*3,(void*)0);
-    // glEnableVertexAttribArray(0);
-
-    // glBindVertexArray(0);
-
-    GLuint new_vao;
-    glGenVertexArrays(1,&new_vao);
-    glBindVertexArray(new_vao);
-    /**
-     * vertex buffer object
-     */
-    GLuint new_vbo;
-    glGenBuffers(1,&new_vbo);
-    /**
-     * element buffer object
-     */
-    GLuint ebo;
-    glGenBuffers(1,&ebo);
-
-    glBindBuffer(GL_ARRAY_BUFFER,new_vbo);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,ebo);
-
-    glBufferData(GL_ARRAY_BUFFER,sizeof(vertices_reg),vertices_reg,GL_STATIC_DRAW);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(indices),indices,GL_STATIC_DRAW);
-
-    /*Configure how shader extract the data from VBO*/
-    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,6 * sizeof(float),(void*)0);
-    glVertexAttribPointer(1,3,GL_FLOAT,GL_FALSE,6 * sizeof(float),(void*)(sizeof(float) * 3));
-
-    glEnableVertexAttribArray(0);
-    glEnableVertexAttribArray(1);
-
-    glBindVertexArray(0);
-    
-    /**
-     * exercises
-     */
-    GLuint ex_vao;
-    glGenVertexArrays(1,&ex_vao);
-    GLuint ex_vbo;
-    glGenBuffers(1,&ex_vbo);
-
-    glBindVertexArray(ex_vao);
-    glBindBuffer(GL_ARRAY_BUFFER,ex_vbo);
-    glBufferData(GL_ARRAY_BUFFER,sizeof(vertices_ex),vertices_ex,GL_STATIC_DRAW);
-    glVertexAttribPointer(0,3,GL_FLOAT,false,6 * sizeof(float),(void*)0);
-    glVertexAttribPointer(1,3,GL_FLOAT,false,6 * sizeof(float),(void*)(3*sizeof(float)));
-    glEnableVertexAttribArray(0);
-    glEnableVertexAttribArray(1);
-    glBindVertexArray(0);
-
-
     /**
      * Information for retangle for texture
      */
@@ -332,22 +202,6 @@ int main(void)
         /* Render here */
         glClearColor(0.5f,0.5f,0.5f,0.1f);
         glClear(GL_COLOR_BUFFER_BIT);
-
-        // float time = glfwGetTime();
-        // float red = (std:: sin(time) + 1) / 2;
-        // GLint location = glGetUniformLocation(shaderProgram,"ourColour");
-        /**
-         * Only changed when the shader is active
-         * Set the active program
-         * Changed the value of the uniform variable
-         */
-        //glUseProgram(shaderProgram);
-        // glUniform4f(location,red,0.0f,0.0f,1.0f);
-        //float time = glfwGetTime();
-        //float value = std::sin(time)*1/2;
-        //s2.use();
-        //s2.setFloat("x_off",value);
-        //s2.setFloat("y_off",value);
         glBindVertexArray(text_vao);
         s3.use();
         /**
@@ -361,38 +215,26 @@ int main(void)
         {
             s3.setInt("myTexture",3);
         }
-        // s3.setInt("myTexture0",2);
-        // s3.setInt("myTexture1",3);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         loop++;
         std::this_thread::sleep_for(std::chrono::seconds(1));
-
-        // glUseProgram(shaderProgram);
-        // glBindVertexArray(vao[0]);
-        // glDrawArrays(GL_TRIANGLES,0,3);
-
-
-        // glBindVertexArray(vao[1]);
-        // glDrawArrays(GL_TRIANGLES,0,3);
-        /* Poll for and process events */
-
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    glDeleteBuffers(1,&new_vbo);
-    glDeleteBuffers(1,&ebo);
-    glDeleteVertexArrays(1,&new_vao);
-
-    glDeleteBuffers(1,&ex_vbo);
-    glDeleteVertexArrays(1,&ex_vao);
-
     glDeleteTextures(1,&texture);
+    glDeleteTextures(1,&texture_1);
+    glDeleteTextures(1,&texture_2);
+    glDeleteTextures(1,&texture_3);
 
     stbi_image_free(data);
     stbi_image_free(data_1);
     stbi_image_free(data_2);
     stbi_image_free(data_3);
+
+    glDeleteBuffers(1,&text_vbo);
+    glDeleteBuffers(1,&text_ebo);
+    glDeleteVertexArrays(1,&text_vao);
 
     glfwTerminate();
     return 0;
